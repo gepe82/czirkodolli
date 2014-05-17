e2dae6889dc914b3254d97c2420722232f0ce801
Peter Geri <gepe82@gmail.com>
2014-05-17 13:19:20 +0200

    Beta 093

--- js/kepek.js
+++ js/kepek.js
@@ -4,6 +4,8 @@ $(document).ready(function(){
         $.getJSON('js/kepek.json', function(obj){
             $.each(obj, function(i, galeria){
                 if ( galeria.pictures.length > 0 ){
+                    $('#accordion_wojs').remove();
+                    $('.js_show').show();
                     $('#accordion').append( $('<h3>', {'innerHTML':galeria.title, 'id': galeria.id } ), $('<div>').append( function(){
                         var html = [];
                         $.each(galeria.pictures, function(j, kep){
@@ -12,17 +14,24 @@ $(document).ready(function(){
                         return html;
                         } ) );
                 }
-            });            
+            });
             $( "#accordion" ).css({
             'margin': '10px 10px 10px 15px',
             'box-shadow': '-1px -1px 5px 2px rgba(0, 0, 0, 0.3)'
             }).accordion({
                 heightStyle: 'content',
-                collapsible: true
+                collapsible: true,
+                active: false
                 });
-            if ( szel > 480 ){
-                $('a[rel^="prettyPhoto"]').prettyPhoto();    
-            }    
+            if ( szel > 560 ){
+                $('a[rel^="prettyPhoto"]').prettyPhoto();
+            }
+
+            /*$('#a_iroda, #a_irodahaz').click(function(e){
+                //e.stopPropagation();
+                $('#accordion').accordion({'active':'false'});
+                $(location.hash).click();
+                });*/
         });
-        
+
 });
