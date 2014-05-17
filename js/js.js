f71c245efa78132985ab2b422138822a9f91f21d
Peter Geri <gepe82@gmail.com>
2014-05-17 13:14:59 +0200

    Beta 08

---
+++ js/js.js
@@ -0,0 +1,14 @@
+$('#email').ready(function(){
+    $('#email').bind('click focus', function(e){
+            var contents = ['czirko', 'dolli', 'gmail', 'com', '.', '@'];
+            var order = [0, 4, 1, 5, 2, 4, 3];
+            var em = '';
+            $.each(order, function(i, value){
+                em += contents[order[i]];
+                });
+            if( $(this).attr('href') == '#' ){
+                e.preventDefault();
+                $(this).fadeOut('slow').attr('href', 'mailto:' + em).text(em).attr('title', '').fadeIn('slow');                
+            }
+        });
+    });
