73d6b853dec82c011bfa52e1f5ef018fa7d91e80
Peter Geri <gepe82@gmail.com>
2014-05-17 13:25:05 +0200

    Version 1.2

--- js/index.js
+++ js/index.js
@@ -3,4 +3,12 @@ $(document).ready(function(){
     $('nav li a').on('click', function(e){
         e.target.click();
     })
+    
+    var contents = ['czirko', 'dolli', 'office', 'english', 'hu', '.', '@', '-'];
+    var order = [0, 1, 6, 2, 7, 3, 5, 4];
+    var em = '';
+    $.each(order, function(i, value){
+        em += contents[order[i]];
+        });    
+    $('#email').attr('href', 'mailto:' + em).text(em).attr('title', '');
 });
