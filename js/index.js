dd3446c18cf96397b5300e8ce9c986d1fb3751c9
Geri Peter <p.geri@mito.hu>
2015-02-08 19:24:24 +0100

    facebook icon, email new format for html, arvaltozas

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     // Ez azért kell bele, mert a forgás miatt 1-2 böngésző nem akarja aktiválni a linket
     $('nav li a').on('click', function(e){
         e.target.click();
-    })
+    });
     
     var contents = ['czirko', 'dolli', 'office', 'english', 'hu', '.', '@', '-'];
     var order = [0, 1, 6, 2, 7, 3, 5, 4];
