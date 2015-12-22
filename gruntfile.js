7b5e54ef927fe2ba903506f8499929f9a6456d18
Geri Peter <p.geri@mito.hu>
2015-12-22 22:52:57 +0100

    reduce number of requests on page images, add grunt tasks

--- gruntfile.js
+++ gruntfile.js
@@ -5,19 +5,26 @@ module.exports = function(grunt) {
 			all: ['js/index.js', 'js/js.js', 'js/kapcsolat.js', 'js/kepek.js', 'js/szolgaltatasok.js']
 		},
 
-		//concat: {
-		//	dist: {
-		//		files: {
-		//			'assets/js/app.js' : ['js/js.js', 'js/kapcsolat.js', 'kepek.js', 'szolgaltatasok.js'],
-		//		}
-		//	}
-		//},
+		'concat': {
+            // concat js file of page images
+            images: {
+                files: {
+                    'assets/js/images.min.js' : ['js/jquery-1.4.2.min.js', 'js/jquery-ui-1.10.3.custom.min.js', 'js/jquery.prettyPhoto-compressed.js', 'js/images.min.js']
+                }
+            }
+		},
 
         uglify: {
-            my_target: {
+            // minify js files of page index
+            index: {
                 files: {
                     'assets/js/index.js': ['js/index.js', 'js/js.js']
-                    //'dest/output2.min.js': ['src/input2.js']
+                }
+            },
+            // minify js files of page images
+            images: {
+                files: {
+                    'js/images.min.js': ['js/kepek.js', 'js/js.js']
                 }
             }
         },
@@ -27,11 +34,24 @@ module.exports = function(grunt) {
 		},
 
 		cssmin: {
+            // minify css file for page index
 			dist: {
 				files: {
 					'assets/css/style.min.css': ['css/stiluslap.css', 'css/media.css']
 				}
-			}
+			},
+
+            // minify css files for pages
+            pages: {
+                files: [{
+                    expand: true,
+                    cwd: 'css/',
+                    src: ['tartalom.css', 'tartalom-dt.css', 'tartalom-l.css'],
+                    dest: 'assets/css/',
+                    ext: '.min.css'
+                }]
+            }
+
 		},
 
 		autoprefixer: {
@@ -69,7 +89,7 @@ module.exports = function(grunt) {
 
 	grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-uglify');
-	//grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-imagemin');
     grunt.loadNpmTasks('grunt-contrib-csslint');
@@ -83,4 +103,14 @@ module.exports = function(grunt) {
         'cssmin',
 		'imagemin'
 	]);
+
+    // task for generating js file of page image
+    grunt.registerTask('page-images', [
+        'uglify:images',
+        'concat:images'
+    ]);
+
+    grunt.registerTask('pages', [
+        'cssmin:pages'
+    ]);
 };
