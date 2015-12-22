ae6c427b43e4a0bcf65cf9ea046abe90ee55b533
Geri Peter <p.geri@mito.hu>
2015-12-22 21:10:45 +0100

    fix tan indents

--- js/szolgaltatasok.js
+++ js/szolgaltatasok.js
@@ -1,13 +1,12 @@
 $(document).ready(function(){
-            $( "#accordion" ).css({
+    $("#accordion")
+        .css({
             'margin': '10px 10px 10px 15px',
             'font-size': '1em'
-            
-            //'box-shadow': '-1px -1px 5px 2px rgba(0, 0, 0, 0.3)'
-            }).accordion({
-                heightStyle: 'content',
-                collapsible: true,
-                active: false
-                });
-        
+        })
+            .accordion({
+            heightStyle: 'content',
+            collapsible: true,
+            active: false
+        });
 });
